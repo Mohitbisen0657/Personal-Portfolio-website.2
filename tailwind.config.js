77ced02306db27fcb072a519db8b6a4db1f2924c
Mohitbisen0657 <bisen0657@gmail.com>
2025-01-16 23:25:31 +0530

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  
   theme: {
     extend: {
+      display: ['contents'],
       transitionTimingFunction : {
         jump: "cubic-bezier(0.68, -0.55, 0-265, 1.55)",
       },
